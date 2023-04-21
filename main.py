a778b4cb5569c878ddd17f5a039cd69ae0f89a47
robotscott94 <scmasanders@gmail.com>
2023-04-20 20:14:42 -0400

    updating api routes

--- main.py
+++ main.py
@@ -1,13 +1,65 @@
 from flask import Flask, jsonify
+from flask_cors import CORS
+import csv
 import os
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+# @app.route('/')
+# def index():
+#     return jsonify({"this is where my flask app would go": "IF I HAD ONE 🚅"})
+
+#################################################
+# Flask Setup
+#################################################
+app = Flask(__name__)
+CORS(app)
+
+
+@app.route("/")
+def welcome():
+    """May Wildlife Sample Data."""
+    return (
+        f"Available Routes:<br/>"
+        f"/api/patients<br/>"
+        f"/api/statuscodes<br/>"
+        f"/api/animals<br/>"
+        f"/api/locations<br/>"
+    )
+
+
+@app.route("/api/patients")
+def patients():
+    with open('resources/patients.csv') as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+    return jsonify(data)
+
+    return jsonify(patient_arr)
+
+@app.route("/api/statuscodes")
+def statuscodes():
+    with open('resources/StatusCodes.csv') as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+    return jsonify(data)
+
+
+@app.route("/api/animals")
+def animals():
+    with open('resources/Animals.csv') as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+    return jsonify(data)
+
+@app.route("/api/locations")
+def locations():
+    with open('resources/Places.csv') as f:
+        reader = csv.DictReader(f)
+        data = [row for row in reader]
+    return jsonify(data)
 
 
 if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
+    app.run(debug=True, port=os.getenv("PORT", default=5001))
